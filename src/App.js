850a6be70dc28c9e1da264fd1f2e5c10af53ef83
decaloms <sebastien.decalom@amersports.com>
2022-03-28 15:59:24 +0200

    test deploiment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 function App() {
     return (
         <div className="App">
+            <div><h1>TOTO</h1></div>
             <header>
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1>We now have Auth!</h1>
